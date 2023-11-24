6944b39b5dadd1a7d9a957ec684b578af2a4488e
kdebord-fhsu <kjdebord@mail.fhsu.edu>
2023-11-23 23:49:04 -0600

    added the date each match was played

--- riotAPI.py
+++ riotAPI.py
@@ -1,13 +1,16 @@
 from riotwatcher import LolWatcher
 from dotenv import load_dotenv
 import os
+from datetime import datetime
+
 
 def setup_env():
     load_dotenv('../../config.env')
     api_key = "RGAPI-b8607081-dd4c-48b7-bc0f-6c6f27544678"
     lol_watcher = LolWatcher(api_key)
-    del(api_key)
+    del (api_key)
     return lol_watcher
+
 
 lol_watcher = setup_env()
 
@@ -20,16 +23,19 @@ player_routing = 'americas'
 try:
     summoner = lol_watcher.summoner.by_name(player_region, player_name)
     match_history = lol_watcher.match.matchlist_by_puuid(region=player_routing, puuid=summoner['puuid'],
-                                                        queue=420,
-                                                        start=0, count=num_matches_data)
+                                                         queue=420,
+                                                         start=0, count=num_matches_data)
 
-    print('Game times:')
+    print('Game times and Dates:')
     for match_id in match_history:
         match_data = lol_watcher.match.by_id(region=player_routing, match_id=match_id)
         game_time_seconds = match_data['info']['gameDuration']
         game_time_minutes = game_time_seconds // 60
         game_time_seconds %= 60
-        print(f"Match ID: {match_id}, Game Time: {game_time_minutes}m {game_time_seconds}s")
+        game_creation_timestamp = match_data['info']['gameCreation'] / 1000  # Convert to seconds
+        game_date = datetime.utcfromtimestamp(game_creation_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+        print(f"Match ID: {match_id}, Game Time: {game_time_minutes}m {game_time_seconds}s, Date: {game_date}")
 
     print('Total Gold Obtained in Each Game:')
     for match_id in match_history:
